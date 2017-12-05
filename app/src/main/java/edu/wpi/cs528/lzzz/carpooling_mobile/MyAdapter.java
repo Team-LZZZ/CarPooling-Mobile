cd32e2dca02c9b66c34742e8fdff2463348e0bff
echomelodylwh <wli6@wpi.edu>
2017-12-05 17:49:12 -0500

    reservation activity created

--- app/src/main/java/edu/wpi/cs528/lzzz/carpooling_mobile/MyAdapter.java
+++ app/src/main/java/edu/wpi/cs528/lzzz/carpooling_mobile/MyAdapter.java
@@ -1,6 +1,7 @@
 package edu.wpi.cs528.lzzz.carpooling_mobile;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +16,7 @@ import butterknife.ButterKnife;
 import edu.wpi.cs528.lzzz.carpooling_mobile.model.CarPool;
 
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
 
     public static class ViewHolder extends RecyclerView.ViewHolder
     {
@@ -25,13 +26,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         public TextView mAddressTextView;
         @Bind(R.id.car_info_textview)
         public TextView mCarInfoTextView;
+        public View view;
 
         public ViewHolder( View v )
 
         {
             super(v);
             ButterKnife.bind(this,v);
-
+            this.view = v;
         }
     }
 
@@ -53,14 +55,22 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder( ViewHolder viewHolder, int i )
+    public void onBindViewHolder(final ViewHolder viewHolder, int i )
     {
         CarPool c = carPools.get(i);
         viewHolder.mOfferTextView.setText(c.getOfferer().getUsername());
         viewHolder.mAddressTextView.setText("From     " + c.getStartLocation().getName() + "       To     "+ c.getTargetLocation().getName());
         viewHolder.mCarInfoTextView.setText(c.getCar().getMake()  + "     "  + c.getCar().getModel() + "     " + c.getCar().getPlate());
 //        viewHolder.mImageView.setImageDrawable(mContext.getDrawable(p.getImageResourceId(mContext)));
+        viewHolder.view.setOnClickListener(new  View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(mContext, ReservationActivity.class);
+                mContext.startActivity(i);
+            }
+        });
     }
+
 
     @Override
     public int getItemCount()
---
+++ app/src/main/java/edu/wpi/cs528/lzzz/carpooling_mobile/ReservationActivity.java
@@ -0,0 +1,22 @@
+package edu.wpi.cs528.lzzz.carpooling_mobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by liweihao on 12/5/17.
+ */
+
+public class ReservationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation);
+//
+//        Intent intent = new Intent(this, LoginActivity.class);
+//        startActivity(intent);
+    }
+}
