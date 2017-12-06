3ce0b84a101c4c613395878959a647deb63cd2fa
Q-Zhao <qzhao@wpi.edu>
2017-12-06 02:34:54 -0500

    login/registration modified

---
+++ app/src/main/java/edu/wpi/cs528/lzzz/carpooling_mobile/handlers/IConnectionStatus.java
@@ -0,0 +1,9 @@
+package edu.wpi.cs528.lzzz.carpooling_mobile.handlers;
+
+/**
+ * Created by QZhao on 12/6/2017.
+ */
+
+public interface ConnectionStatus {
+    public void onComplete();
+}
---
+++ app/src/main/java/edu/wpi/cs528/lzzz/carpooling_mobile/utils/CommonUtils.java
@@ -0,0 +1,8 @@
+package edu.wpi.cs528.lzzz.carpooling_mobile.utils;
+
+/**
+ * Created by QZhao on 12/6/2017.
+ */
+
+public class CommonUtils {
+}
