8fa946c09be51454963f985d031702baddad1af5
echomelodylwh <wli6@wpi.edu>
2017-12-13 15:44:39 -0500

    Update Profile Activity

--- app/src/main/java/edu/wpi/cs528/lzzz/carpooling_mobile/ProfileActivity.java
+++ app/src/main/java/edu/wpi/cs528/lzzz/carpooling_mobile/ProfileActivity.java
@@ -1,19 +1,245 @@
 package edu.wpi.cs528.lzzz.carpooling_mobile;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.Point;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import java.io.File;
+import java.util.Locale;
+import java.util.Random;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import edu.wpi.cs528.lzzz.carpooling_mobile.model.AppContainer;
+import edu.wpi.cs528.lzzz.carpooling_mobile.model.User;
+import edu.wpi.cs528.lzzz.carpooling_mobile.utils.CommonConstants;
+import edu.wpi.cs528.lzzz.carpooling_mobile.utils.CommonUtils;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    @Bind(R.id.profile_imageView)
+    ImageView mImageView;
+    @Bind(R.id.profile_imageButton)
+    ImageButton mImageButton;
+    private TransferUtility transferUtility;
+    private TransferObserver transferObserver;
+    private static final int REQUEST_PHOTO= 2;
+    private ProgressDialog progressDialog;
+    private static final String TAG = "UploadActivity";
+    private User user;
+    long time;
+    Random random = new Random();
+    private File mPhotoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        ButterKnife.bind(this);
+        user = AppContainer.getInstance().getActiveUser();
+        mPhotoFile = getPhotoFile(user);
+        transferUtility = CommonUtils.getTransferUtility(this);
 
+        final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        PackageManager packageManager = this.getPackageManager();
+        boolean canTakePhoto = mPhotoFile != null &&
+                captureImage.resolveActivity(packageManager) != null;
+        mImageButton.setEnabled(canTakePhoto);
 
-
+        if (canTakePhoto) {
+            Uri uri = Uri.fromFile(mPhotoFile);
+            captureImage.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        }
+        mImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(captureImage, REQUEST_PHOTO);
+            }
+        });
+        getProfile(user.getPhoto());
 
     }
 
+    private void beginUpload(String filePath) {
 
+//        Runnable runnable = new Runnable(){
+//            @Override
+//            public void run() {
+//                deleOldPhoto(mPhotoFile.getName() + time);
+//                Log.i(CommonConstants.LogPrefix, "delete");
+//            }
+//        };
+//        new Thread(runnable).start();
+        if (filePath == null) {
+            Toast.makeText(this, "Could not find the filepath of the selected file",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        File file = new File(filePath);
+        TransferObserver observer = transferUtility.upload(CommonConstants.BUCKET_NAME, file.getName() + time,
+                file);
+        observer.setTransferListener(new UploadListener());
+        user.setPhoto(CommonConstants.S3_PREFIX + mPhotoFile.getName() + time);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        time = System.currentTimeMillis();
+        progressDialog = CommonUtils.createProgressDialog(this, "Loading...");
+        progressDialog.show();
+        if (requestCode == REQUEST_PHOTO) {
+            new android.os.Handler().postDelayed(
+                    new Runnable() {
+                        public void run() {
+
+                            beginUpload(mPhotoFile.getPath());
+                            updatePhotoView();
+                            progressDialog.dismiss();
+                        }
+                    }, 3000);
+
+        }
+    }
+
+    public File getPhotoFile(User user) {
+        File externalFilesDir = this
+                .getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        if (externalFilesDir == null) {
+            return null;
+        }
+
+        return new File(externalFilesDir, getPhotoFilename(user));
+    }
+
+    private void updatePhotoView() {
+        if (mPhotoFile == null || !mPhotoFile.exists()) {
+            mImageView.setImageDrawable(null);
+        } else {
+            Bitmap bitmap = getScaledBitmap(
+                    mPhotoFile.getPath(), this);
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+            bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+            mImageView.setImageBitmap(bitmap);
+        }
+    }
+
+    public static Bitmap getScaledBitmap(String path, Activity activity) {
+        Point size = new Point();
+        activity.getWindowManager().getDefaultDisplay()
+                .getSize(size);
+
+        return getScaledBitmap(path, size.x, size.y);
+    }
+
+    public static Bitmap getScaledBitmap(String path, int destWidth, int destHeight) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(path, options);
+
+        float srcWidth = options.outWidth;
+        float srcHeight = options.outHeight;
+
+        int inSampleSize = 1;
+        if (srcHeight > destHeight || srcWidth > destWidth) {
+            if (srcWidth > srcHeight) {
+                inSampleSize = Math.round(srcHeight / destHeight);
+            } else {
+                inSampleSize = Math.round(srcWidth / destWidth);
+            }
+        }
+
+        options = new BitmapFactory.Options();
+        options.inSampleSize = inSampleSize;
+
+        return BitmapFactory.decodeFile(path, options);
+    }
+
+    private class UploadListener implements TransferListener {
+
+        @Override
+        public void onError(int id, Exception e) {
+            Log.e(TAG, "Error during upload: " + id, e);
+        }
+
+        @Override
+        public void onProgressChanged(int id, long bytesCurrent, long bytesTotal) {
+            Log.d(TAG, String.format("onProgressChanged: %d, total: %d, current: %d",
+                    id, bytesTotal, bytesCurrent));
+        }
+
+        @Override
+        public void onStateChanged(int id, TransferState newState) {
+            Log.d(TAG, "onStateChanged: " + id + ", " + newState);
+        }
+    }
+
+    private void getProfile(String photoPath){
+        Glide.with(this).load(photoPath).diskCacheStrategy(DiskCacheStrategy.ALL).listener(new RequestListener() {
+            @Override public boolean onException(Exception e, Object model, Target target, boolean isFirstResource) {
+                android.util.Log.d(CommonConstants.LogPrefix, String.format(Locale.ROOT,
+                        "onException(%s, %s, %s, %s)", e, model, target, isFirstResource), e);
+                return false;
+            }
+            @Override public boolean onResourceReady(Object resource, Object model, Target target, boolean isFromMemoryCache, boolean isFirstResource) {
+                android.util.Log.d(CommonConstants.LogPrefix, String.format(Locale.ROOT,
+                        "onResourceReady(%s, %s, %s, %s, %s)", resource, model, target, isFromMemoryCache, isFirstResource));
+                return false;
+            }
+        }).error(R.drawable.wpi).into(mImageView);
+    }
+
+
+    private void deleOldPhoto(String keyName){
+        try {
+            CommonUtils.getS3Client(this).deleteObject(new DeleteObjectRequest(CommonConstants.BUCKET_NAME, keyName));
+        } catch (AmazonServiceException ase) {
+            Log.i(CommonConstants.LogPrefix,"Caught an AmazonServiceException.");
+            Log.i(CommonConstants.LogPrefix,"Error Message:    " + ase.getMessage());
+            Log.i(CommonConstants.LogPrefix,"HTTP Status Code: " + ase.getStatusCode());
+            Log.i(CommonConstants.LogPrefix,"AWS Error Code:   " + ase.getErrorCode());
+            Log.i(CommonConstants.LogPrefix,"Error Type:       " + ase.getErrorType());
+            Log.i(CommonConstants.LogPrefix,"Request ID:       " + ase.getRequestId());
+        } catch (AmazonClientException ace) {
+            Log.i(CommonConstants.LogPrefix,"Caught an AmazonClientException.");
+            Log.i(CommonConstants.LogPrefix,"Error Message: " + ace.getMessage());
+        }
+    }
+
+    public String getPhotoFilename(User user) {
+        return "IMG_" + user.getUsername();
+    }
 }
+
--- app/src/main/java/edu/wpi/cs528/lzzz/carpooling_mobile/utils/CommonConstants.java
+++ app/src/main/java/edu/wpi/cs528/lzzz/carpooling_mobile/utils/CommonConstants.java
@@ -8,8 +8,8 @@ public class CommonConstants {
 
     // connections
 //    public static String BASE_URL = "http://youzhou1993.pythonanywhere.com/api/";
-    public static String BASE_URL = "http://10.0.2.2:5000/api/";
-    //public static String BASE_URL = "http://e1069146.ngrok.io/api/";
+ //   public static String BASE_URL = "http://10.0.2.2:5000/api/";
+    public static String BASE_URL = "https://8132e714.ngrok.io/api/";
     public static String registration = "reg";
     public static String login = "login";
     public static String getAllCarpools = "carPools";
@@ -22,4 +22,10 @@ public class CommonConstants {
     // GoogleMap
     public static int MAP_CAMERA = 15;
 
+    //Amazon S3
+    public static final String COGNITO_POOL_ID = "us-east-2:d59f7b37-b24b-4c30-bcf2-e9aef1c51965";
+    public static final String COGNITO_POOL_REGION = "us-east-2";
+    public static final String BUCKET_NAME = "wpics528";
+    public static final String BUCKET_REGION = "us-east-2";
+    public static final String S3_PREFIX = "https://s3.us-east-2.amazonaws.com/wpics528/";
 }
